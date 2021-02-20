35370daa86884d00c60b7cb6a608bc2c3b7ad3d0
max <maxpower@riseup.net>
2021-02-20 16:44:41 -0300

    Validaciones funcionales

--- angular/unidad2/src/app/app.module.ts
+++ angular/unidad2/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -18,7 +19,9 @@ import { DetalleComponent } from './detalle/detalle.component';
     DetalleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/unidad2/src/app/registro/registro.component.ts
+++ angular/unidad2/src/app/registro/registro.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
+
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  formularioDeRegistro:FormGroup
+
+  constructor(private fb:FormBuilder) {
+    this.formularioDeRegistro = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required] ,
+      telefono: '',
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(6)]]
+    })
+  }
 
   ngOnInit(): void {
   }
-
 }
