d249d30c0cdc4f36520439a5a300bca9454071be
max <maxpower@riseup.net>
2021-03-06 17:43:55 -0300

    Unidad 3 Funcional

---
+++ angular/unidad3/src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductosService } from '../productos.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  productos:any = []
+  constructor(private productosSrv:ProductosService) {
+    this.productosSrv.getProductos()
+    .subscribe(data=>{
+      this.productos=data 
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular/unidad3/src/app/productos.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  constructor(private http:HttpClient) { }
+  getProductos(){
+  return this.http.get("https://jsonfy.com/items")
+  }
+}
