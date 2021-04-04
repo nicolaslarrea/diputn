f172f6522695efa11c606a2a2742f1f2020d7d84
max <maxpower@riseup.net>
2021-04-04 19:22:24 -0300

    Ejercicio de manejo de estados

---
+++ react/modulo2/unidad1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg'
+import './App.css'
+import Producto from './Producto'
+
+function App() {
+  return (
+    <div className="App">
+      <Producto/>
+    </div>
+  );
+}
+
+export default App;
